b9303548683c6ab793d73a8466335fc316a9b4e0
liuyuren <1540979440@qq.com>
2025-08-25 21:26:27 +0800

    feat: 5

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,21 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild',
+    sourcemap: false,
+    // 禁用 gzip 压缩大小报告，可略微减少打包时间
+    reportCompressedSize: false,
+    // 规定触发警告的 chunk 大小
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        // Static resource classification and packaging
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
 })
